eb72f52bfa2bb3e079d2b03b512e2f0ca3fc1817
Seer <50992478+dxy-Seer@users.noreply.github.com>
2020-10-12 18:53:30 +0200

    UPDATE

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     const m = await message.channel.send("ping?");
     m.edit(`Pong! ${m.createdTimestamp - message.createdTimestamp}ms`); 
-    console.log('Ping command used in: ' + message.guild.id)
 }
 
 module.exports.help = {
