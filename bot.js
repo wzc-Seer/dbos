ed4877f7f39184da487418dc49c82c37bc469f30
Seer <50992478+dxy-Seer@users.noreply.github.com>
2020-10-12 18:52:01 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -6,40 +6,52 @@ const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 bot.on('ready', async () => {
-    console.log('MAIN SHARD ONLINE')
-
-    bot.user.setStatus('idle');
-})
-
-
-fs.readdir("./commands/", (err, files) => {
-    if(err) console.log(err);
-
-    let jsfile = files.filter(f => f.split(".").pop() === "js")
-        if(jsfile.length <= 0) {
-            console.log("i was unable to find any commands!");
-        }
-
-        jsfile.forEach((f) => {
-            let props = require(`./commands/${f}`);
-            console.log(`SHARD: [${f}] IS ONLINE`);
-            bot.commands.set(props.help.name, props);
-            props.help.aliases.forEach(alias => {
-                bot.aliases.set(alias, props.help.name);
-            })
+    console.log('MAIN SHARD ONLINE\n-------------------------')
+    if(config.bot.commandLogging == true){
+        console.log('Command logging is now enabled!\n-------------------------');
+    } else {
+        console.log('Command logging is not enabled!\n-------------------------')
+    }
+    if(config.bot.messageLogging == true){
+        console.log('Message logging is now enabled!');
+    } else {
+        console.log('Message logging is not enabled!')
+    }
+    console.log('----[STARTING SHARDS]----')
+    bot.user.setStatus('dnd');
+    function StartShards() {
+        fs.readdir("./commands/", (err, files) => {
+            if(err) console.log(err);
+        
+            let jsfile = files.filter(f => f.split(".").pop() === "js")
+                if(jsfile.length <= 0) {
+                    console.log("ERROR: There is not an existing [commands] folder OR there are no files in the [commands] folder!");
+                }
+        
+                jsfile.forEach((f) => {
+                    let props = require(`./commands/${f}`);
+                    console.log(`SHARD: [${f}] IS ONLINE\n-------------------------`);
+                    bot.commands.set(props.help.name, props);
+                    props.help.aliases.forEach(alias => {
+                        bot.aliases.set(alias, props.help.name);
+                    })
+                })
         })
+    }
+    setTimeout(StartShards, 3000);
 })
 bot.on("message", async message => {
+    var messageAuthor = message.member.user.tag;
+    if(config.bot.messageLogging == true){console.log('ML -> [' + message.guild.name + '] -> ' + messageAuthor + ': ' + message.content)}
     if(message.channel.type === "dm") return;
     if(message.author.bot) return;
-    if(!message.content.startsWith(config.prefix)) return;
-    let args = message.content.slice(prefix.length).trim().split(/ +/g);
+    if(!message.content.startsWith(config.bot.prefix)) return;
+    let args = message.content.slice(config.bot.prefix.length).trim().split(/ +/g);
     let cmd;
     cmd = args.shift().toLocaleLowerCase();
-    let commandfile = bot.commands.get(cmd.slice(prefix.length));
+    let commandfile = bot.commands.get(cmd.slice(config.bot.prefix.length));
     if(commandfile) commandfile.run(bot, message, args);
-
-    // run commands
+    if(config.bot.commandLogging == true){console.log('CL ->' + message.content + ' command used in: [' + message.guild.name + '] - By: ' + messageAuthor)}
     if(bot.commands.has(cmd)) {
         command = bot.commands.get(cmd);
     } else if (bot.aliases.has(cmd)) {
@@ -51,18 +63,4 @@ bot.on("message", async message => {
         return;
     }
 })
-
-
-	// if (command === 'ping') {
-	// 	message.reply('Pong!');
-	// } else if (command === 'log') {
-	// 	message.channel.send('logged to console!');
-	// 	console.log('log cmd executed');
-	// }
-	// else if (message.content === `${prefix}server`) {
-	// 	message.channel.send(`Server: ${message.guild.name}\nMembers: ${message.guild.memberCount}`);
-	// }
-	
-
-
-bot.login(config.token);
+bot.login(config.bot.token);
--- guides.js
+++ guides.js
@@ -1,5 +1,3 @@
-/* This section of code is used by the install and setup guides, if you modify it make sure you still are hosting a webpage (seen in "show") or Uptime Robot will stop working */
-
 const express = require("express");
 const router = express.Router();
 const discordBotkit = require("botkit-discord");
@@ -8,49 +6,19 @@ const Config = require('./config.json');
 
 
 
-
-router.get("/domainname", function(request, response) {
-  let domain = process.env.PROJECT_DOMAIN;
-  response.status(200).json({
-    message: domain
-  });
-});
-
-router.get("/botinfo", async function(request, response) {
-  let authURL;
-  let domain = process.env.PROJECT_DOMAIN;
-  let uptime = process.uptime();
-
-  try {
-    authURL =
-      "https://discordapp.com/api/oauth2/authorize?client_id=" +
-      Config.bot.id +
-      "&permissions=8&scope=bot";
-  } catch (e) {
-    console.error(e);
-  }
-
-  response.status(200).json({
-    url: authURL,
-    domain: domain,
-    uptime: uptime
-  });
-});
-
-
 router.get("/", function(request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
-router.get("/use", function(request, response) {
-  response.redirect("https://www.npmjs.com/package/@dxy_seer/easydiscord");
+router.get("/invite", function(request, response) {
+  response.redirect("https://discordapp.com/api/oauth2/authorize?client_id="+Config.bot.id +"&permissions=8&scope=bot");
 });
-router.get("/npm", function(request, response) {
-  response.redirect("https://www.npmjs.com/package/@dxy_seer/easydiscord");
+router.get("/easter", function(request, response){
+  response.sendFile(__dirname + "/views/easteregg.html");
 });
 // if 404
 router.get("*", function(request, response) {
   response.sendFile(__dirname + "/views/errors/404.html");
 });
-
-
+            
+console.log('------------[ACTIVATING]-------------\nSHARD: guides.js ONLINE - This is a standalone shard!\n-------------------------')
 module.exports = router;
