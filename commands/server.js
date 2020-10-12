274212d811a4bf1576c3c9784ab29511cb7e7de5
Seer <50992478+dxy-Seer@users.noreply.github.com>
2020-10-12 20:12:25 +0200

    Init

--- commands/server.js
+++ commands/server.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-    const m = await message.channel.send("Getting server stats!");
+    const m = await message.channel.send("Getting server stats...");
     m.edit(`Server: ${message.guild.name}\nMembers: ${message.guild.memberCount}`); 
 }
 
